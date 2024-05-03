8d58acf647a7f15f4096b02a880b0468939b631d
jysndabuio <jysndabu@outlook.com>
2024-05-04 00:35:54 +0200

    Partial Solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,21 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {code} valid?");
+    if code.chars().any(|x| x.is_ascii_alphabetic() || !x.is_ascii_alphanumeric()) || code.replace(" ", "").len() == 1 {
+        return false;
+    }
+        
+    let code_num:Vec<u32> = code.chars()
+            .filter(|c| c.is_ascii_digit())
+            .map(|x| x.to_digit(10).unwrap())
+            .collect();
+    
+
+    let sum: u32 = code_num.iter().rev().enumerate().step_by(2).map(|(i, &x)| {
+        match i {
+            _ if x * 2 > 9 => x * 2 - 9,
+            _ => x * 2,
+        }
+    }).sum();
+
+    sum % 10 == 0
 }
