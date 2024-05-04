6c31551074f74198c680e8d5b5ad0d5ddcd6d72a
jysndabuio <jysndabu@outlook.com>
2024-05-04 14:10:08 +0200

    All passed except one

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
     let code_no_whitespace = code.replace(" ", "");
+    dbg!("Invalid code detected:", &code_no_whitespace);
 
     if code_no_whitespace.chars().any(|x| !x.is_ascii_digit()) || 
         code_no_whitespace.len() <= 1  || 
@@ -11,16 +12,22 @@ pub fn is_valid(code: &str) -> bool {
     let code_num:Vec<u32> = code_no_whitespace.chars()
             .filter_map(|x| x.to_digit(10))
             .collect();
+    dbg!("Invalid code detected:", &code_num);
     
-
-    let sum: u32 = code_num.iter().rev().step_by(2).map(|&x| {
-        let doubled = x * 2;
-        if doubled > 9 {
-            doubled - 9
+    
+    let sum: u32 = code_num.iter().rev().enumerate().map(|(i, &x)| {
+        if i % 2 != 0 {
+            let doubled = x * 2;
+            if doubled > 9 {
+                doubled - 9
+            } else {
+                doubled
+            }
         } else {
-            doubled
+            x
         }
     }).sum();
+    dbg!("Invalid code detected:", &sum);
 
     sum % 10 == 0
 }
